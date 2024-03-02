62416c28e3a1b22adac48529f7947f60f3fe90b0
Willian Gonçalves <willian.p.g@outlook.com>
2024-03-02 12:16:14 -0300

    Requisito 07 - Implemente o método registerClientAddress

--- src/main/java/com/betrybe/minhaconta/presentation/Application.java
+++ src/main/java/com/betrybe/minhaconta/presentation/Application.java
@@ -1,6 +1,7 @@
 package com.betrybe.minhaconta.presentation;
 
 import com.betrybe.minhaconta.business.EnergyAccount;
+import com.ions.lightdealer.sdk.model.Address;
 import com.ions.lightdealer.sdk.model.Client;
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
 import com.ions.lightdealer.sdk.service.LightDealerApi;
@@ -82,6 +83,14 @@ public class Application {
    * Req. 7 – Register client address.
    */
   public void registerClientAddress() {
+    String cpf = ui.inputClientCpf();
+    Client client = api.findClient(cpf);
+    if (client == null) {
+      ui.showMessage("Pessoa cliente não encontrada!");
+    }
+    Address address = new Address();
+    ui.fillAddressData(address);
+    api.addAddressToClient(address, client);
   }
 
   /**
